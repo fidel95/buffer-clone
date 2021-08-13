ca1f9301620ba50fdae177896bc401d594ae2638
Fidel Castro <fc.fidel1995@gmail.com>
2021-08-12 21:45:12 -0400

    added login,registration,forgot password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,5 @@
 class MainController < ApplicationController
     def index
-        flash[:notice] = "Hello"
-        flash[:alert] = "fail"
     end
 
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    # render plain: "thanks"
+    # render plain: params[:user]
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Succesfully created account"
+    else
+      # flash[:alert] = "something went wrong"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,:password,:password_confirmation)
+  end
+end
